19494162c01a872eec25bb436e72f749a7ddd2fc
Jonathan Peppers <jonathan.peppers@hitcents.com>
2017-01-26 17:05:51 -0600

    Revert "[XamlC] provide backward compat for ProvideValueTarget without Target… (#583)"
    This reverts commit e5036833036c4de5b49a401869381b09f459a6eb.

--- Xamarin.Forms.Xaml/XamlServiceProvider.cs
+++ Xamarin.Forms.Xaml/XamlServiceProvider.cs
@@ -136,11 +136,6 @@ namespace Xamarin.Forms.Xaml.Internals
 		readonly object[] objectAndParents;
 		readonly object targetProperty;
 
-		[Obsolete("TargetProperty is now supported, use it")]
-		public SimpleValueTargetProvider(object[] objectAndParents) : this (objectAndParents, null)
-		{
-		}
-
 		public SimpleValueTargetProvider(object[] objectAndParents, object targetProperty)
 		{
 			if (objectAndParents == null)
